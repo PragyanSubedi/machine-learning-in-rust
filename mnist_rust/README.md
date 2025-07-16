f8d23d339370583f6f3f6b7d66bc0e61821d3181
Pragyan Subedi <pragyansubedi@gmail.com>
2025-07-16 18:49:54 +0545

    Updated mnist training code

---
+++ mnist_rust/README.md
@@ -0,0 +1,7 @@
+Run the trainer using:
+
+```
+cargo clean # To clean (optional)
+cargo build
+cargo run --release
+```
---
+++ mnist_rust/src/main.rs
@@ -0,0 +1,22 @@
+#![recursion_limit = "512"]  
+mod model;
+mod training;
+mod data;
+use crate::{model::ModelConfig, training::TrainingConfig};
+use burn::{
+    backend::{Autodiff, Wgpu},
+    optim::AdamConfig,
+};
+
+fn main() {
+    type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
+
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    crate::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
+}
